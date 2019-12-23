153141281710530bd18d972303d7c2ca18a6f6e1
cale96 <a.cale96@gmail.com>
2019-12-23 17:55:48 +0100

    working version

--- ArrowFinder/src/arrow_finder/src/arrow_finder_main.cpp
+++ ArrowFinder/src/arrow_finder/src/arrow_finder_main.cpp
@@ -1,5 +1,6 @@
 #include "arrow_finder_computation.hpp"
 
+
 #include <ros/ros.h>
 #include <image_transport/image_transport.h>
 #include <cv_bridge/cv_bridge.h>
@@ -7,22 +8,24 @@
 #include <sensor_msgs/fill_image.h>
 #include <opencv2/opencv.hpp>
 #include <opencv2/highgui/highgui.hpp>
-
+#include <list>
 #include <tf/transform_listener.h>
 #include <tf/transform_broadcaster.h>
 #include <tf/LinearMath/Quaternion.h>
 #include <tf/LinearMath/Transform.h>
+#include "/home/serl/SERL_Project/Eigen_library/Eigen/Dense"
+
+//#include <star_marker_msgs/Marker.h>
+//#include <star_marker_msgs/MarkerArray.h>
 
 ArrowFinder finder;
 
-ros::CallbackQueue *callback_queue;
-ros::Subscriber img_info_sub;
-ros::Subscriber img_sub;
-
 int image_height, image_width;
+ros::CallbackQueue *callback_queue;
+ros::Subscriber info_sub;
+ros::Subscriber img_sub;
 bool info_received = false;
 
-// ================== CALLBACK FUNCTIONS ==================
 void infoCallback(const sensor_msgs::CameraInfo::ConstPtr& msg) {
 	sensor_msgs::CameraInfo info_msg = *msg;
 	if(! info_received) {
@@ -35,7 +38,15 @@ void infoCallback(const sensor_msgs::CameraInfo::ConstPtr& msg) {
 void imageCallback(const sensor_msgs::Image::ConstPtr& msg) {
 	sensor_msgs::Image img_msg = *msg;
   	try {
-		finder.setImage(cv_bridge::toCvShare(msg, "bgr8")->image, image_height, image_width);
+		list<freccia> pippo = finder.findArrows(cv_bridge::toCvShare(msg, "bgr8")->image);
+		const freccia* oo = finder.getBiggestArrow(pippo);
+		if(oo != nullptr){
+			VectorXf x(4);
+			x = finder.worldCoordinates(oo);
+			cout<<"Coordinate World: \n"<<x<<endl;
+		}
+ 
+
   	} catch (cv_bridge::Exception& e) {
     	ROS_ERROR("Could not convert from '%s' to 'bgr8'.", msg->encoding.c_str());
   	}  
@@ -43,34 +54,33 @@ void imageCallback(const sensor_msgs::Image::ConstPtr& msg) {
 
 int main(int argc, char **argv)
 {
-  	ros::init(argc, argv, "arrow_finder_main");
+  	ros::init(argc, argv, "spot_finder_main");
   	ros::NodeHandle nh;
 
-	// Create the callback queue;
+	//create the callback queue;
 	callback_queue = new ros::CallbackQueue();
 
-	// Camera image info subscriber
-	ros::SubscribeOptions ops_info = ros::SubscribeOptions::create<sensor_msgs::CameraInfo>(
-					"/camera/camera_info", // Tpic name
-					10, // Queue length
-					infoCallback, // Callback function called when informations of images are available
-					ros::VoidPtr(), // Tracked object, we don't need one thus NULL
-					callback_queue // Pointer to callback queue object
+	// Camera Info subscriber
+	/*ros::SubscribeOptions ops_info = ros::SubscribeOptions::create<sensor_msgs::CameraInfo>(
+					"/camera/camera_info", // topic name
+					10, // queue length
+					infoCallback, // callback
+					ros::VoidPtr(), // tracked object, we don't need one thus NULL
+					callback_queue // pointer to callback queue object
 			);
-	img_info_sub = nh.subscribe(ops_info);
-	img_info_sub = nh.subscribe("/camera/camera_info", 10, infoCallback);
+	info_sub = nh.subscribe(ops_info);
+	info_sub = nh.subscribe("/camera/camera_info", 10, infoCallback);*/
 
 	// Camera Image subscriber
 	ros::SubscribeOptions ops_img = ros::SubscribeOptions::create<sensor_msgs::Image>(
-					"/camera/image_raw_throttle", // Topic name
-					10, // Queue length
-					imageCallback, // Callback function called when an image is available
-					ros::VoidPtr(), // Tracked object, we don't need one thus NULL
-					callback_queue // Pointer to callback queue object
+					"/camera/image_raw_throttle", // topic name
+					10, // queue length
+					imageCallback, // callback
+					ros::VoidPtr(), // tracked object, we don't need one thus NULL
+					callback_queue // pointer to callback queue object
 			);
 	img_sub = nh.subscribe(ops_img);
 	img_sub = nh.subscribe("/camera/image_raw_throttle", 10, imageCallback);
-
 	ros::spin();
   return 0;
 }
